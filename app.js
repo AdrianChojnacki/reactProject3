e248f4efe9e493a3f5734f404455154ca9e8eeb9
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-01 15:18:00 +0200

    base

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-class ListItems extends React.Component {}
+class ListItems extends React.Component {
+  state = {
+    items: ["jabłko", "śliwka", "gruszka"],
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.state.items.map((item) => (
+          <li key={item}>{`owoc ${item}`}</li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 ReactDOM.render(<ListItems />, document.querySelector(`#root`));
