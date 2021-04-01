a9fefa4c81d83bffdcef3c6dfaf8dd096aaae1b9
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-01 15:47:49 +0200

    filter

--- app.js
+++ app.js
@@ -4,38 +4,49 @@ const data = {
       id: 1,
       age: 29,
       name: "Arek",
+      sex: "male",
     },
     {
       id: 2,
       age: 49,
       name: "Marta",
+      sex: "female",
     },
     {
       id: 3,
       age: 19,
       name: "Stasia",
+      sex: "female",
     },
     {
       id: 4,
       age: 24,
       name: "Karol",
+      sex: "male",
     },
   ],
 };
 
 const Item = ({ user }) => (
-  <div>
+  <div className="userInfo">
     <h1>Użytkownik {user.name}</h1>
-    <h2>Ma {user.age} lat</h2>
+    <p>Informacje o użytkowniku:</p>
+    <p>
+      Wiek użytkownika: <strong>{user.age}</strong>
+    </p>
+    <p>Płeć użytkownika: {user.sex}</p>
   </div>
 );
 
 class ListItems extends React.Component {
+  state = {};
+
   render() {
-    const users = this.props.data.users;
+    let users = this.props.data.users;
+    users = users.filter((user) => user.sex === "female");
     const Items = users.map((user) => <Item key={user.id} user={user} />);
 
-    return <ul>{Items}</ul>;
+    return <>{Items}</>;
   }
 }
 
