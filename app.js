d1dbd55541bfa2c513660861a8cd1835343551f1
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-01 15:38:07 +0200

    users

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-class ListItems extends React.Component {
-  state = {
-    items: ["jabłko", "śliwka", "gruszka"],
-  };
+const data = {
+  users: [
+    {
+      id: 1,
+      age: 29,
+      name: "Arek",
+    },
+    {
+      id: 2,
+      age: 49,
+      name: "Marta",
+    },
+    {
+      id: 3,
+      age: 19,
+      name: "Stasia",
+    },
+    {
+      id: 4,
+      age: 24,
+      name: "Karol",
+    },
+  ],
+};
 
+const Item = ({ user }) => (
+  <div>
+    <h1>Użytkownik {user.name}</h1>
+    <h2>Ma {user.age} lat</h2>
+  </div>
+);
+
+class ListItems extends React.Component {
   render() {
-    return (
-      <ul>
-        {this.state.items.map((item) => (
-          <li key={item}>{`owoc ${item}`}</li>
-        ))}
-      </ul>
-    );
+    const users = this.props.data.users;
+    const Items = users.map((user) => <Item key={user.id} user={user} />);
+
+    return <ul>{Items}</ul>;
   }
 }
 
-ReactDOM.render(<ListItems />, document.querySelector(`#root`));
+ReactDOM.render(<ListItems data={data} />, document.querySelector(`#root`));
